b64d0e00bab66a89b3ef411e20eb094f0e37f2ba
Akhil Kshirasagara <ksakhil1996@gmail.com>
2020-06-13 19:35:25 +0530

    changed the icon and name of our app

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/es/integration/react'
 
-import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import { store, persistor } from './redux/store'
@@ -18,4 +17,3 @@ ReactDOM.render(
     </BrowserRouter></Provider>,
   document.getElementById('root')
 );
-
