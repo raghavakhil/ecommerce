b0cc4981d1c4cc71fac61cfd87715017916f5d64
Akhil Kshirasagara <ksakhil1996@gmail.com>
2020-06-17 12:06:34 +0530

    cart items will not be displayed until and unless user signs in

--- src/components/cart-item/cart-item.component.js
+++ src/components/cart-item/cart-item.component.js
@@ -1,15 +1,25 @@
 import React from 'react';
+import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
 
 import './cart-item.styles.scss'
+import { selectCurrentUser } from '../../redux/user/user.selector'
 
-const CartItem = ({ item: { imageUrl, name, price, quantity } }) => (
+const CartItem = ({ item: { imageUrl, name, price, quantity } , currentUser}) => (
     <div className='cart-item'>
-        <img src={imageUrl} alt='item' />
-        <div className='item-details'>
+        {
+            currentUser ? (
+                <img src={imageUrl} alt='item' />,
+               <div className='item-details'>
             <span className='name'>{name}</span>
 <span className='price'>{quantity} * ${price}</span>
-        </div>
+        </div>) : null
+        }       
     </div>
 )
 
-export default CartItem;
+const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser
+})
+
+export default connect(mapStateToProps)(CartItem);
